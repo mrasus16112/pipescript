1df993aebe057bf3b3ecbdca593473d693c3c170
Yondoku <69292387+Yondoku@users.noreply.github.com>
2022-11-06 22:32:13 -0900

    removed return statements because esolang

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import {readFileSync} from 'fs';
 import ohm from 'ohm-js';
+import {toAST} from 'ohm-js/extras';
 
 if (Math.random() < 1/20) console.error("ERR 000\nYou pitiful coward. Try again.");
 const grammar = ohm.grammar(String(readFileSync("pipescript.ohm")));
-
-
-
-
-
 
