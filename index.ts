c507f6c9b16d4528d6e96653bf9cef0239d409a8
Yondoku <69292387+Yondoku@users.noreply.github.com>
2022-11-08 00:01:12 -0900

    holy moly

--- index.ts
+++ index.ts
@@ -2,6 +2,18 @@ import {readFileSync} from 'fs';
 import ohm from 'ohm-js';
 import {toAST} from 'ohm-js/extras';
 
-if (Math.random() < 1/20) console.error("ERR 000\nYou pitiful coward. Try again.");
-const grammar = ohm.grammar(String(readFileSync("pipescript.ohm")));
+const read = (path: string) => String(readFileSync(path));
+function error(code: number, message: string) {
+    console.error(`ERR ${code}\n${message}`);
+    process.exit(code);
+}
 
+if (Math.random() < 1/20) error(1, "I will murder you.");
+
+const grammar = ohm.grammar(read("pipescript.ohm"));
+const match = grammar.match(read(process.argv[2])); // temporary dirty fix lmao
+
+if (match.failed()) error(2, "Did you fail English class?");
+
+const mapping = {};
+const syntaxTree = toAST(match, mapping);
